1a6b59eb2fe2840508531d60ffc73e7bf7e03e37
sMailund <simen.m.s@hotmail.com>
2021-03-23 23:50:32 +0100

    work on poc

---
+++ cmd/authenticate.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/zmb3/spotify"
+	"log"
+	"net/http"
+)
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadCurrentlyPlaying, spotify.ScopeUserReadPlaybackState, spotify.ScopeUserModifyPlaybackState)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
+
+var client *spotify.Client
+var playerState *spotify.PlayerState
+
+func Authenticate() {
+
+	http.HandleFunc("/callback", completeAuth)
+
+	go func() {
+		url := auth.AuthURL(state)
+		fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+		// wait for auth to complete
+		client = <-ch
+
+		// use the client to make calls that require authorization
+		user, err := client.CurrentUser()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("You are logged in as:", user.ID)
+
+		playerState, err = client.PlayerState()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Found your %s (%s)\n", playerState.Device.Type, playerState.Device.Name)
+	}()
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
+}
